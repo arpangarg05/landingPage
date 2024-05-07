592102db4379957e260f8618811f52a5c4ccf743
Arpan Garg <Arpangarg_se20b16_70@dtu.ac.in>
2024-05-08 03:14:46 +0530

    Home v1.1.1

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='flex flex-col mt-20 h-[70dvh] border-b-[1px] border-neutral-900 '>
+    <div className='flex flex-col mt-20 h-[70vh] border-b-[1px] border-neutral-900 '>
         <div className='mx-auto w-[90%]  sm:border-dashed border-0 lg:w-4/5'>
             <div className=' h-[62px] mx-auto border-b-[1px] border-dashed border-neutral-800'></div>
-            <h1 className='mx-auto w-[80%] text-neutral-300 text-center p-0 m-0 font-extrabold border-b-[1px] border-dashed border-neutral-700 text-6xl md:text-7xl lg:text-[7vw] '>The Dev of the Web</h1>
+            <h1 className='mx-auto w-[100%] lg:w-[80%] text-neutral-300 text-center p-0 m-0 font-extrabold border-b-[1px] border-dashed border-neutral-700 text-6xl md:text-7xl lg:text-[7vw] '>The Dev of the Web</h1>
             <div className=' mx-auto p-1 border-[1px] border-t-0 border-neutral-800 border-opacity-90 border-dashed'>
                 <h1 className='w-[90%]  lg:w-[60%] text-center text-neutral-400 font-light mx-auto my-10 lg:text-[1.3vw] md:text-[2vw] text-[2.5vw] '>Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.</h1>
             </div>
